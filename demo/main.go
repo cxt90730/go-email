ddf7365eb89e87f157e819cbe7c5d4b92a14f00a
cuixiaotian <cuixiaotian@xunlei.com>
2017-12-26 18:06:43 +0800

    go email

--- demo/main.go
+++ demo/main.go
@@ -7,7 +7,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	msg := s.NewMessage("cxt", "test", "test content", []string{"service@onebooktech.com"}, []string{"service"})
+	msg := s.NewMessage("cxt", "test", "test content", []string{"cxt@onebooktech.com"}, []string{"cxt"})
 	err = s.SendMail(msg, "")
 	if err != nil {
 		panic(err)
